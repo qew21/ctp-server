25e18ccdd1ff6f402873dbd511af41cda23c35d0
qew21 <illking@foxmail.com>
2024-01-31 21:57:42 +0800

    修改请求地址

--- client/src/utils/Request.js
+++ client/src/utils/Request.js
@@ -5,7 +5,10 @@ axios.defaults.timeout = 90000000;
 
 // post请求头
 axios.defaults.headers['Content-Type'] = 'application/json';
-var baseURL = "http://127.0.0.1:7000/";
+var host = window.location.hostname; // 获取域名（不包括端口）
+var protocol = window.location.protocol; // 获取协议（例如 'http:' 或 'https:'）
+var port = "7000"; // 设置新端口
+var baseURL = protocol + "//" + host + ":" + port + "/"; // 拼接新的 baseURL
 const api = axios.create({
   baseURL: baseURL, // 设置为你的后端服务器地址
 });
